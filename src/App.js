61302978f391bf18e30f95b0a412ec79fe871e76
yuumami97 <yuumami97@gmail.com>
2020-11-20 12:36:35 +0000

    more

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
+
 import "./App.css";
 
 import Sidebar from "./Sidebar";
+import Chat from "./Chat";
 
 function App() {
   return (
     <div className="app">
       <div className="app__body">
         <Sidebar />
-        /* Chat */
+        <Chat />
       </div>
     </div>
   );
--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -3,20 +3,31 @@ import "./SidebarChat.css";
 
 import { Avatar } from "@material-ui/core";
 
-function SidebarChat() {
+function SidebarChat({ addNewChat }) {
   const [seed, setSeed] = useState("");
   
   useEffect(()=>{
     setSeed(Math.floor(Math.random()*5000));
   }, []);
 
-  return (
+  const createChat = () => {
+    const roomName = prompt("Please enter name for chat"); 
+    if (roomName) {
+      //Do DB stuffs
+    }
+  }
+
+  return !addNewChat ? (
   <div className="sidebarChat">
     <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
     <div className="sidebarChat__info">
       <h2>Room name</h2>
       <p>Last message ...</p>
     </div>
+  </div>
+  ) : (
+  <div className="sidebarChat" onClick={ createChat }>
+    <h2>Add new chat</h2>
   </div>
   );
 }
